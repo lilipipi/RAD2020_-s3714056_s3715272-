1a551c6f94fda170590133b194965456de1ad99b
Hung Tran <s3715272@rmit.edu.vn>
2020-05-26 16:21:03 +1000

    fixed host in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'quiet-wildwood-56338.herokuapp.com'
+  host = 'floating-brushlands-50723.herokuapp.com'
   config.action_mailer.default_url_options = {host: host }
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
